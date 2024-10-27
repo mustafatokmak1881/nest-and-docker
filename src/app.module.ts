51f4e090b79aa154145d8d1dce59f1e08f122669
mehmet.tokmak <mehmet.tokmak@beyn.com.tr>
2024-10-27 03:02:15 +0300

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'nest-test-db',
+      entities: [],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
