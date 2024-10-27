209b4479ea733a706d88426f38ae98df30bf6d79
mehmet.tokmak <mehmet.tokmak@beyn.com.tr>
2024-10-27 03:30:55 +0300

    .

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,10 +7,18 @@ import { User } from './users/user.entity';
 export class UserService {
   constructor(
     @InjectRepository(User)
-    private userRepository: Repository<User>,
+    private usersRepository: Repository<User>,
   ) {}
 
   findAll(): Promise<User[]> {
-    return this.userRepository.find();
+    return this.usersRepository.find();
+  }
+
+  findOne(id: number): Promise<User | null> {
+    return this.usersRepository.findOneBy({ id });
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.usersRepository.delete(id);
   }
 }
