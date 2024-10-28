3bc370d2becd9cba1af573d9b695e1793afc8253
mehmet.tokmak <mehmet.tokmak@beyn.com.tr>
2024-10-28 19:09:32 +0300

    .

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { UserService } from './user.service';
+import { User } from './users/user.entity';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
   @Get()
-  getUsers(): any {
+  getUsers(): Promise<User[]> {
     return this.userService.findAll();
+  }
+
+  @Get(':id')
+  getUser(@Param('id') id: number): Promise<User> {
+    return this.userService.findOne(id);
   }
 }
